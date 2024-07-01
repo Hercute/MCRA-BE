08f24b0f03666c0b04239e7c94131d85752b470e
tsdnhts <jyjangtop@hanmail.net>
2024-07-01 14:32:02 +0900

    초기 설정

---
+++ src/main/kotlin/com/hercute/mcrabe/McraBeApplication.kt
@@ -0,0 +1,11 @@
+package com.hercute.mcrabe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class McraBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<McraBeApplication>(*args)
+}
